f1bfe9b4d99e68bad75184eece6ce59554a53d85
Sahil Obhrai <82256818+sahilobhrai@users.noreply.github.com>
2022-03-01 16:25:09 +0530

    Create Sort Colors.cpp

---
+++ Sort Colors.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int c1=0,c2=0,c3=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0) c1++;
+            if(nums[i]==1) c2++;
+            if(nums[i]==2) c3++;
+        }
+        for(int i=0;i<c1;i++)
+            nums[i]=0;
+        
+        for(int i=c1;i<c2+c1;i++)
+            nums[i]=1;
+        
+        for(int i=c2+c1;i<c1+c2+c3;i++)
+            nums[i]=2;
+    
+    }
+};
