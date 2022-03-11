ef091ab9efad6b429f8f774642ef0f2fc1538070
Sahil Obhrai <82256818+sahilobhrai@users.noreply.github.com>
2022-03-11 10:42:24 +0530

    Create creating a linked list.cpp

---
+++ Linked List/creating a linked list.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ class Node{
+   public:
+      int data;
+      Node* next;
+ };
+ 
+ void printList(Node* n){
+   while(n!=NULL){
+     cout<<n->data<<" ";
+     n=n->next;
+   }
+ }
+
+int main() 
+{   
+    Node* head=NULL;
+    Node* two=NULL;
+   
+    head=new Node();
+    two=new Node();
+    
+    head->data=1;
+    head->next=two;
+    
+    two->data=2;
+    two->next=NULL;
+    
+    printList(head);
+    return 0;
+}
